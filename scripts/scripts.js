0dd62669a8465daf1ddb49a9bef99eed99153f5c
seeyd7 <szymon8866@gmail.com>
2020-12-06 17:09:43 +0100

    Siemka

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,25 +8,25 @@ const navbar = document.querySelector("nav");
 
 
 
-// addEventListener("scroll", () => {
-// 	let scroll = this.scrollY;
-// 	if(scroll > menu.clientHeight) {
-//     go2Top.style.opacity = 1;
-//     go2Top.style.lineHeight = "40px";
-// 		go2Top.style.cursor = "pointer";
-// 		console.log(go2Top.style.cursor);
-// 	navbar.style.height = "8vh";
-// 	} else {
-// 		go2Top.style.opacity = 0;
-// 		go2Top.style.cursor = "default";
-// 		navbar.style.height = "10vh";
-// 		console.log(go2Top.style.cursor);
-// 	}
-// })
+addEventListener("scroll", () => {
+	let scroll = this.scrollY;
+	if(scroll > menu.clientHeight) {
+    go2Top.style.opacity = 1;
+    go2Top.style.lineHeight = "40px";
+		go2Top.style.cursor = "pointer";
+		console.log(go2Top.style.cursor);
+	navbar.style.height = "8vh";
+	} else {
+		go2Top.style.opacity = 0;
+		go2Top.style.cursor = "default";
+		navbar.style.height = "10vh";
+		console.log(go2Top.style.cursor);
+	}
+})
 
-// go2Top.addEventListener("click", () => {
-// 	if(go2Top.style.opacity != 0) window.scrollTo(0, 0);
-// })
+go2Top.addEventListener("click", () => {
+	if(go2Top.style.opacity != 0) window.scrollTo(0, 0);
+})
 
 menuToggler.addEventListener("click", () => {
 	if(menuToggler.classList.contains("active")) {
