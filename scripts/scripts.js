6d363e0bd838722a204642b9e10a4715ba07ee77
seeyd7 <szymon8866@gmail.com>
2020-12-07 12:43:55 +0100

    Siemka

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -37,3 +37,18 @@ menuToggler.addEventListener("click", () => {
 		navMenu.classList.add("active");
 	}
 });
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "flex") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "flex";
+  }
+  });
+}
